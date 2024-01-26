6e059f10ebe22d51162c9d09c3c551c5c90b1a12
shivssm <shivssm95@gmail.com>
2024-01-26 15:26:44 +0530

    DB Connection Setup

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ const port = process.env.PORT || 3000;
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'root',
-    database : 'smart-brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl: {rejectUnauthorized: false},
+    host : process.env.DATABASE_HOST,
+    user : process.env.DATABASE_USER,
+    password : process.env.DATABASE_PW,
+    database : process.env.DATABASE_DB
   }
 });
 
